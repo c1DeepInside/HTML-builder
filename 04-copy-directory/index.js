fafae3528afe6a205ea7a1a68e5387cdce03dd88
c1DeepInside <vanya0759@gmail.com>
2022-10-31 20:50:16 +0300

    feat: solve fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+copyDir();
+
+async function copyDir(){
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+    if (err) throw err;
+  });
+
+  const files = await fsPromises.readdir(path.join(__dirname, 'files-copy'), err => {
+    if (err) throw err;   
+  });
+  files.forEach(file => {
+    fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+      if (err) throw err;
+    });
+  }); 
+
+  const copyFiles = await fsPromises.readdir(path.join(__dirname, 'files'),(err, files) => {
+    if (err) throw err;
+  });
+  copyFiles.forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err => {
+      if (err) throw err});
+  }); 
+}
