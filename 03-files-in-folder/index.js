cb8d06e976878bea1ac9458d5b6ab9ab323adfdc
c1DeepInside <vanya0759@gmail.com>
2022-11-01 15:11:49 +0300

    fix: correct work with files with a dot

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,9 +4,10 @@ fsPromises.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true})
   for (let i = 0; i < files.length; i++){
     if (files[i].isFile()){
       const dir = path.join(__dirname, 'secret-folder', files[i].name);
-      const name = path.basename(dir).split('.');
+      const name = path.basename(dir);
+      const extname = path.extname(dir);
       fsPromises.stat(dir).then(file => {
-        console.log(name[0] + ' - ' + name[1] + ' - ' + file.size + 'b');
+        console.log(name.slice(0, name.length - extname.length) + ' - ' + extname.slice(1) + ' - ' + file.size + 'b');
       });
     }
   }
